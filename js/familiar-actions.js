ba4556e5ebbb6e689d9cec0d006c7123a2736738
Eve Kuusk <evie.kuusk@gmail.com>
2015-09-26 20:23:26 -0400

    Started on attack and magic attack scripts

--- js/familiar-actions.js
+++ js/familiar-actions.js
@@ -1,9 +1,12 @@
 // ***** ------ FAMILIAR ACTIONS ------ ***** //
 
   // *** ---  GLOBAL VARIABLES --- *** //
-    // BATTLE
-    var attack
-    var magicAttack
+    var attackDamage
+    var magicAttackDamage
+
+    var enemyAttackDamage
+    var enemyMagicAttackDamage
+
 
     var defend
     var magicDefend
@@ -11,3 +14,37 @@
     var specialAttack
 
     var flee
+
+    var heroStartHealth
+    var enemyStartHealth
+
+    var heroDamageTaken
+    var enemyDamageTaken
+
+
+  // *** ---  FIGHT --- *** //
+    // hero attacks
+    function heroAttack() {
+      console.log(heroFamiliar.description.toUpperCase() + " will do damage between " + (heroFamiliar.strength + 1) + " and " + (heroFamiliar.strength + 10) + ".");
+      attackDamage = heroFamiliar.strength + (Math.floor(Math.random() * 10) + 1) // adds 1 - 10 to familiar's existing strength stat
+      return attackDamage
+    }
+
+    function heroMagicAttack() {
+      console.log(heroFamiliar.description.toUpperCase() + " will do magic damage between " + ( heroFamiliar.strength + (heroFamiliar.magic * 1)) + " and " + (heroFamiliar.strength + (heroFamiliar.magic * 10 )) + ".");
+      magicAttackDamage = heroFamiliar.strength + (heroFamiliar.magic * (Math.floor(Math.random() * 10) + 1)) // adds magic stat multiplied by a random number between 1 - 10 to familiar's existing strength stat
+      return magicAttackDamage
+    }
+
+    // enemy attacks
+    function enemyAttack() {
+      console.log("Enemy " + enemyFamiliar.description.toUpperCase() + " will do damage between " + (enemyFamiliar.strength + 1) + " and " + (enemyFamiliar.strength + 10) + ".");
+      enemyAttackDamage = enemyFamiliar.strength + (Math.floor(Math.random() * 10) + 1) // adds 1 - 10 to enemy familiar's existing strength stat
+      return enemyAttackDamage
+    }
+
+    function enemyMagicAttack() {
+      console.log("Enemy " + enemyFamiliar.description.toUpperCase() + " will do magic damage between " + ( enemyFamiliar.strength + (enemyFamiliar.magic * 1)) + " and " + (enemyFamiliar.strength + (enemyFamiliar.magic * 10 )) + ".");
+      enemyMagicAttackDamage = enemyFamiliar.strength + (heroFamiliar.magic * (Math.floor(Math.random() * 10) + 1)) // adds magic stat multiplied by a random number between 1 - 10 to enemy familiar's existing strength stat
+      return enemyMagicAttackDamage
+    }
